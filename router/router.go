d0e8af4db6e84ff0c3d9c7e0e47c79e913401055
hafidkrntn <hafidkrntn@gmail.com>
2025-07-24 21:44:31 +0700

    push hehe

--- router/router.go
+++ router/router.go
@@ -1,16 +1,25 @@
 package router
 
 import (
-	"backend-go/middleware"
 	"backend-go/internal/router"
+	"backend-go/middleware"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Sesuaikan dengan asal request FE kamu
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	api := r.Group("/api/v1")
 	{
